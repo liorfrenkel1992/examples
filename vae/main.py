909d06c513e3037aa5893e44de34dcafedafb5f0
liorfrenkel1992 <64976963+liorfrenkel1992@users.noreply.github.com>
2020-09-15 17:35:26 +0300

    Update main.py

--- vae/main.py
+++ vae/main.py
@@ -63,9 +63,7 @@ class VAE(nn.Module):
     def decode(self, z, istrain=True):
         h3 = F.relu(self.fc3(z))
         #return torch.sigmoid(self.fc4(h3))
-        h41 = self.fc41(h3)
-        h42 = self.fc42(h3)
-        return F.relu(h41), F.relu(h42)
+        return F.sigmoid(self.fc41(h3)), F.sigmoid(self.fc42(h3))
       
     def svdsqrtm(self, x, eps=1e-15):
         #Return the matrix square root of x calculating using the svd.
