023d52b83692be00b9c7705163d56a13eb294364
liorfrenkel1992 <64976963+liorfrenkel1992@users.noreply.github.com>
2020-09-17 11:59:58 +0300

    Update main.py

--- vae/main.py
+++ vae/main.py
@@ -15,7 +15,7 @@ parser.add_argument('--use_UT', action='store_true', default=False,
                     help='the model uses unscented transformation for sampling')
 parser.add_argument('--batch-size', type=int, default=128, metavar='N',
                     help='input batch size for training (default: 128)')
-parser.add_argument('--epochs', type=int, default=1, metavar='N',
+parser.add_argument('--epochs', type=int, default=122, metavar='N',
                     help='number of epochs to train (default: 10)')
 parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='enables CUDA training')
