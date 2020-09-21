b0ae114e6fd4bb2cafb94d6f68a996633a92043c
liorfrenkel1992 <64976963+liorfrenkel1992@users.noreply.github.com>
2020-09-21 17:24:15 +0300

    Update main.py

--- vae/main.py
+++ vae/main.py
@@ -174,7 +174,6 @@ class VAE(nn.Module):
             #diff = diff_x + diff_z1 - diff_z2
             diff = diff_x + diff_z1
             pq_sum = (p_x_z*p_z)/q_z_x
-            print(pq_sum)
             #big_pq = torch.zeros_like(pq_sum).to(device)
             #for i in range(bs):
             #    if diff[i] >= -10:
