23aa76d8999d819a1de4efc5f6b15212e819d675
liorfrenkel1992 <64976963+liorfrenkel1992@users.noreply.github.com>
2020-09-21 15:13:59 +0300

    Update main.py

--- vae/main.py
+++ vae/main.py
@@ -169,8 +169,9 @@ class VAE(nn.Module):
             #p_x_z, diff_x = self.norm_dist(x, mu_x, var_x, x_exps_max)
             p_x_z = torch.exp(torch.sum(x * torch.log(mu_x) + (1 - x) * torch.log(1 - mu_x), dim=1) - x_exps_max)
             p_z, diff_z1 = self.norm_dist(sample, torch.zeros(bs, sample.shape[1]).to(device), torch.ones(bs, sample.shape[1]).to(device), z1_exps_max)
-            q_z_x, diff_z2 = self.norm_dist(sample, mu_z, var_z, z2_exps_max)
-            print(diff_z2)
+            #q_z_x, diff_z2 = self.norm_dist(sample, mu_z, var_z, z2_exps_max)
+            q_z_x = torch.exp(self.norm_dist_exp(sample, mu_z, var_z))
+            print(q_z_x)
             #diff = diff_x + diff_z1 - diff_z2
             diff = diff_z1 - diff_z2
             pq_sum = (p_x_z*p_z)/q_z_x
