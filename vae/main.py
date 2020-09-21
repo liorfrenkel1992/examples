712e4ca34b2fc245fab48c3d9e5fe5d166e77a29
liorfrenkel1992 <64976963+liorfrenkel1992@users.noreply.github.com>
2020-09-21 13:45:43 +0300

    Update main.py

--- vae/main.py
+++ vae/main.py
@@ -296,7 +296,7 @@ def loss_function(recon_x, x, mu, var):
     # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
     # https://arxiv.org/abs/1312.6114
     # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-    KLD = -0.5 * torch.sum(1 + torch.log(var) - mu.pow(2) - var)
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
 
     return BCE + KLD
     
