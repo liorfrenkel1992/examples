9d23e250f1989e4c3a182fa75d3825b44cfb6e26
liorfrenkel1992 <64976963+liorfrenkel1992@users.noreply.github.com>
2020-09-22 12:41:07 +0300

    Update main.py

--- vae/main.py
+++ vae/main.py
@@ -185,7 +185,7 @@ class VAE(nn.Module):
             #q_z_x, diff_z2 = self.norm_dist(sample, mu_z, var_z, z2_exps_max)
             q_z_x = self.norm_dist_exp(sample, mu_z, var_z)
             #diff = diff_x + diff_z1 - diff_z2
-            diff = diff_w1 + diff_x + diff_z1
+            diff = diff_x + diff_z1
             y = w1_exp + p_x_z + p_z - q_z_x
             pq_sum = torch.exp(y - x0) - 1 
             #big_pq = torch.zeros_like(pq_sum).to(device)
@@ -200,7 +200,7 @@ class VAE(nn.Module):
         #C = (-x.shape[1]/2)*math.log(2*math.pi)
         #D = (1/2)*(torch.sum(logvar_z, dim=1) + logvar_z.shape[1])
         
-        return -(w1_exps_max + x_exps_max + z1_exps_max + x0 + torch.log(pq_sum_tensor))
+        return -(x_exps_max + z1_exps_max + x0 + torch.log(pq_sum_tensor))
         #return -(x_exps_max + z1_exps_max - z2_exps_max + torch.log((1/K)*pq_sum_tensor))
         #return -(C + x_exps_max + z1_exps_max - z2_exps_max + torch.log((1/K)*pq_sum_tensor))
         #return C + D + x_exps_max + z_exps_max + torch.log((1/K)*pq_sum_tensor)
